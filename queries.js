3ae0b9d42ee104d242b98734a938cca6e213933c
padmapriya114 <46240280+padmapriya114@users.noreply.github.com>
2021-04-30 15:16:20 +0530

    Add files via upload

---
+++ queries.js
@@ -0,0 +1,81 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'postgres',
+    host: 'api.cny7tsngrm4b.us-east-2.rds.amazonaws.com',
+    database: 'api',
+    password: 'blitz2021',
+    port: 5432,
+})
+
+
+
+const getCountries = (request, response) => {
+  pool.query('SELECT * FROM countries ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+
+const getCountryById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM countries WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+
+const createCountry = (request, response) => {
+  const { name, capital } = request.body
+
+  pool.query('INSERT INTO countries (name, capital) VALUES ($1, $2)', [name, capital], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send('A new country has been added to the database')
+  })
+}
+
+
+
+const updateCountry = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, capital } = request.body
+
+  pool.query(
+    'UPDATE countries SET name = $1, capital = $2 WHERE id = $3',
+    [name, capital, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send('Country has been updated in the database')
+    }
+  )
+}
+
+
+const deleteCountry = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM countries WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`Country deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+  getCountries,
+  getCountryById,
+  createCountry,
+  updateCountry,
+  deleteCountry,
+}
